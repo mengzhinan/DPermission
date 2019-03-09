61a9d7cdd2b91ed29e517a070e8dcadc5bfab7de
duke <136239173@qq.com>
2019-03-09 10:50:43 +0800

    init

---
+++ DPermission/src/main/java/com/duke/dpermission/my/D.java
@@ -0,0 +1,10 @@
+package com.duke.dpermission.my;
+
+/**
+ * @Author: duke
+ * @DateTime: 2019-03-08 22:54
+ * @Description:
+ *  https://www.jianshu.com/p/66d386684d0f?tdsourcetag=s_pctim_aiomsg
+ */
+public class D {
+}
